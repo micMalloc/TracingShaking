2b511eeb78b0f06499a65d0b8546672154363497
micMalloc <hesue615@naver.com>
2017-05-30 19:56:02 +0900

    Add initializing Data Function

--- app/src/main/java/com/example/ativ/tracingshaking/MainActivity.java
+++ app/src/main/java/com/example/ativ/tracingshaking/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     public String name, phNum;
     public Button goInfo;
     public Button popInfo;
+    public Button initInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         goInfo = (Button)findViewById(R.id.inform);
         popInfo = (Button)findViewById(R.id.popInform);
+        initInfo = (Button)findViewById(R.id.initInform);
 
         name = pref.getString("name", null);
         phNum = pref.getString("phoneNum", null);
@@ -45,6 +47,29 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, CustomerActivity.class);
                 startActivity(i);
+            }
+        });
+        initInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences pref = getSharedPreferences("Shake", Activity.MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+
+                /*editor.remove("name");
+                editor.remove("phoneNum");
+                editor.clear();*/
+                editor.putString("name", null);
+                editor.putString("phoneNum", null);
+                editor.commit();
+
+                String name, phNum;
+                name = pref.getString("name", null);
+                phNum = pref.getString("phoneNum", null);
+
+                if (name == null || phNum == null) {
+                    Intent i = new Intent(MainActivity.this, InfoActivity.class);
+                    startActivity(i);
+                }
             }
         });
     }
