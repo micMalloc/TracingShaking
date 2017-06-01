dfd147f4eb6281282ee1f08b8766e61ba7e13f4d
micMalloc <hesue615@naver.com>
2017-06-01 20:04:27 +0900

    QRMaker version 1

--- app/src/main/java/com/example/ativ/tracingshaking/QRMaker.java
+++ app/src/main/java/com/example/ativ/tracingshaking/QRMaker.java
@@ -3,10 +3,57 @@ package com.example.ativ.tracingshaking;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
 
 /**
  * Created by ATIV on 2017-06-01.
  */
 public class QRMaker extends  Activity{
 
+    private Bitmap bitmap = null;
+
+    public String getData () {
+        String data;
+        String name, phNum;
+        SharedPreferences pref = getSharedPreferences("Shake", Activity.MODE_PRIVATE);
+
+        name = pref.getString("name", null); phNum = pref.getString("phone", null);
+        if (name == null || phNum == null)
+            data = null;
+        else
+            data = name + "#" + phNum;
+
+        return data;
+    }
+
+    public Bitmap getBitmap (String data) {
+        MultiFormatWriter gen = new MultiFormatWriter();
+
+        try {
+            final int WIDTH = 400;
+            final int HEIGHT = 400;
+
+            data = new String(data.getBytes("UTF-8"), "ISO-8859-1");
+            BitMatrix byteMap = gen.encode(data, BarcodeFormat.QR_CODE, WIDTH, HEIGHT);
+            Bitmap bitmap = Bitmap.createBitmap(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888);
+
+            for (int i = 0; i < WIDTH; ++i)
+                for (int j = 0; j < HEIGHT; ++j)
+                    bitmap.setPixel(i, j, byteMap.get(i, j) ? Color.BLACK : Color.WHITE);
+
+            ImageView viw = (ImageView) findViewById(R.id.qrView);
+            viw.setImageBitmap(bitmap);
+            viw.invalidate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return bitmap;
+        }
+    }
 }
