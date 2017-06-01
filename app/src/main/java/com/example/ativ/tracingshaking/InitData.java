92e6c0d80a775d11e5d897de521a93f36486ac3f
micMalloc <hesue615@naver.com>
2017-06-01 20:00:47 +0900

    Create InitData and QRMaker Class

---
+++ app/src/main/java/com/example/ativ/tracingshaking/InitData.java
@@ -0,0 +1,10 @@
+package com.example.ativ.tracingshaking;
+
+/**
+ * Created by ATIV on 2017-06-01.
+ */
+public class InitData {
+    public String name, phNum;
+
+
+}
---
+++ app/src/main/java/com/example/ativ/tracingshaking/QRMaker.java
@@ -0,0 +1,12 @@
+package com.example.ativ.tracingshaking;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+
+/**
+ * Created by ATIV on 2017-06-01.
+ */
+public class QRMaker extends  Activity{
+
+}
