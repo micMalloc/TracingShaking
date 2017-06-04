1e7ef1f3d9117f13f97268b64ea8c544434f08d1
micMalloc <hesue615@naver.com>
2017-06-04 18:12:06 +0900

    Create ContactManager Class for Updating Contacts in Mobile Phone

---
+++ app/src/main/java/com/example/ativ/tracingshaking/ContactManager.java
@@ -0,0 +1,58 @@
+package com.example.ativ.tracingshaking;
+
+import android.app.Activity;
+import android.content.ContentProviderOperation;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ATIV on 2017-06-04
+ */
+public class ContactManager extends Activity {
+
+    private ArrayList<ContentProviderOperation> ops;
+    private String displayName;
+    private String mobileNumber;
+
+    public void saveContact (String name, String phNum) {
+
+        displayName = name;
+        mobileNumber = phNum;
+
+        ops = new ArrayList<>();
+        ops.add(ContentProviderOperation.newInsert(
+                ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                .build());
+        if (displayName != null) {
+            ops.add(ContentProviderOperation.newInsert(
+                    ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                    .withValue(
+                            ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
+                            displayName).build());
+        }
+        if (mobileNumber != null) {
+            ops.add(ContentProviderOperation.
+                    newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, mobileNumber)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                            ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                    .build());
+        }
+        try {
+            // 실제저장
+            getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
