b2efc4f0f09e6beed2777493d36003ce1f60a372
micMalloc <hesue615@naver.com>
2017-06-26 15:50:11 +0900

    Create the ScreenOnBroadcastReceiver Class

---
+++ app/src/main/java/com/example/ativ/tracingshaking/ScreenOnBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.example.ativ.tracingshaking;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by ATIV on 2017-06-26.
+ */
+public class ScreenOnBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
